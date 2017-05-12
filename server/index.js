79e2ea3ec3a332d2c5366b3cdb0c989a238d948b
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-05-12 11:06:51 +0700

    add error handling

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const { json, send } = require("micro");
 const { authentication } = require("bukalapak-api");
 
 const createSession = async (req, res) => {
-  const { username, password } = await json(req);
-  const { token } = await authentication.getApiToken({
-    username,
-    password
-  });
-  send(res, 200, token);
+  try {
+    const { username, password } = await json(req);
+    const { token } = await authentication.getApiToken({
+      username,
+      password
+    });
+    send(res, 200, { token });
+  } catch (error) {
+    send(res, 500, {
+      error,
+      message: "not cool bro"
+    });
+  }
 };
 
 module.exports = router(post("/session", createSession));
